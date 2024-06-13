de98f8c4da8112d4b8211d966914a732ebc6a0e9
Sergio <sergio.bmonsalve@gmail.com>
2024-06-13 15:03:28 +0200

    Se modifica la estetica de la landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PuntuacionComponent } from './components/puntuacion/puntuacion.componen
 import { RankingComponent } from './components/ranking/ranking.component';
 import { PlayerConfigComponent } from './components/players/player-config/player-config.component';
 import { RoundSummaryComponent } from './components/summary/summary.component';
+import { InstruccionesComponent } from './components/instrucciones/instrucciones.component';
 
 
 
@@ -12,9 +13,10 @@ import { RoundSummaryComponent } from './components/summary/summary.component';
 const routes: Routes = [
   { path: 'puntuacion', component: PuntuacionComponent, data: {animation: 'PuntuacionPage' }},
   { path: 'player-config', component: PlayerConfigComponent, data: {animation: 'PlayerConfigPage'} },
-  { path: '', component: LandingPageComponent, data: {animation: 'LandingPage'} },  // La ruta raíz dirigirá a la landing page
+  { path: '', component: LandingPageComponent},  // La ruta raíz dirigirá a la landing page
   { path: 'ranking', component: RankingComponent, data: {animation: 'RankingPage' }},
-  { path: 'summary', component: RoundSummaryComponent},
+  { path: 'summary', component: RoundSummaryComponent, data: {animation: 'RoundSummaryPage'}},
+  { path: 'instrucciones', component: InstruccionesComponent,data: {animation: 'instruccionesPage' }},
 
   // Otras rutas de la aplicación...
 ];
